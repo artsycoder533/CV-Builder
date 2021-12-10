fff3505874acaa74f8966146864196b90e187124
Natasha <njohnson533@gmail.com>
2021-12-10 12:29:52 -0500

    added download button to Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Container, Title } from './style';
+import { Container, Title, StyledDownloadIcon } from './style';
+import { DownloadButton } from '../Button/style';
 
 const Header = (props) => {
     const { view } = props;
     return (
-        <Container>
-            <Title>CV Builder</Title>
-        </Container>
-    )
+      <Container>
+        <Title>CV Builder</Title>
+        {view === 4 ? (
+          <DownloadButton className="hide" onClick={() => window.print()}>
+            {" "}
+            <StyledDownloadIcon />
+          </DownloadButton>
+        ) : (
+          ""
+        )}
+      </Container>
+    );
 }
 export default Header;
