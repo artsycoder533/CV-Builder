f9a451ccaf6616a8ace04d27f3040a74bc1d1991
Natasha <njohnson533@gmail.com>
2021-12-10 12:09:44 -0500

    added state variables for App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
-import './App.css';
-import React, { useState, useEffect } from 'react';
-
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import uniqid from "uniqid";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  //state variables
+  const [general, setGeneral] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    portfolio: "",
+    linkedIn: "",
+    github: "",
+  });
+  const [education, setEducation] = useState([
+    {
+      school: "",
+      major: "",
+      startDate: "",
+      endDate: "",
+      gpa: "",
+      minor: "",
+      attending: "",
+      graduate: "",
+      degree: "",
+      designation: "",
+      id: uniqid(),
+    },
+  ]);
+  const [experience, setExperience] = useState([
+    {
+      title: "",
+      company: "",
+      startDate: "",
+      endDate: "",
+      employed: "",
+      duties: "",
+      id: uniqid(),
+    },
+  ]);
+  const [skills, setSkills] = useState([]);
+  const [view, setView] = useState(0);
+  
+  return <div className="App"></div>;
 }
 
 export default App;
