b7449e759befd58275ff7c50c8d3ae30d092eec1
Natasha <njohnson533@gmail.com>
2021-12-10 13:08:55 -0500

    added DisplaySelect component and styled component

---
+++ src/components/DisplaySelect/DisplaySelect.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyledLabel, StyledOption, StyledSelect } from "./style";
+import uniqid from "uniqid";
+
+const DisplaySelect = (props) => {
+    const { label, value, name, handleInput, index, options } = props;
+    return (
+      <React.Fragment>
+        <StyledLabel>{label}</StyledLabel>
+        <StyledSelect
+          name={name}
+          value={value}
+          onChange={(e) => handleInput(e, index)}>
+          <StyledOption disabled value="">
+            {"Select Level"}
+          </StyledOption>
+          {options.map((level) => {
+            return <option key={uniqid()}>{level}</option>;
+          })}
+        </StyledSelect>
+      </React.Fragment>
+    );
+};
+
+export default DisplaySelect;
---
+++ src/components/DisplaySelect/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const StyledLabel = styled.label`
+  font-size: 1.1rem;
+  margin-right: 1rem;
+  font-family: "Poppins", sans-serif;
+`;
+
+export const StyledSelect = styled.select`
+  padding: 0.5rem;
+  margin-right: 1rem;
+  font-family: "Poppins", sans-serif;
+  font-size: 1rem;
+`;
+
+export const StyledOption = styled.option`
+  padding: 0.5rem;
+  font-family: "Poppins", sans-serif;
+`;
+
+export const Container = styled.div`
+  border: 1px solid red;
+`;
